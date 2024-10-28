87ba149931e2b45f7d0ac038834c82008aff6d67
Rosmén Valencia Ferrer <167766152+RosmenPro@users.noreply.github.com>
2024-10-28 14:56:53 +0100

    Delete Clase 34 Vite - Con crono - locastorage/pairs directory

--- Clase 34 Vite - Con crono - locastorage/pairs/src/main.js
+++
@@ -1,9 +0,0 @@
-import Game from './class/Game.js';
-import '../sass/main.scss';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const gameElement = document.getElementById('game');
-  const resetButton = document.getElementById('reset');
-  
-  new Game(gameElement, resetButton);
-});
